fbf95629a23cf60da6db336c409888b20084581f
Meriem Zhang <Meriem.zhang@expensya.com>
2021-12-24 10:42:25 +0100

    first push

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { render } from "react-dom";
 import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>,
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/WeeklyCalendar.js
@@ -0,0 +1,64 @@
+import React, { useEffect } from 'react'
+import "./WeeklyCalendar.css";
+import { useState } from "react";
+import WeeklyCalendarRenderer from "../helpers/WeeklyCalendarRenderer";
+import WeeklyEvents from "../components/WeeklyEvents";
+import { format, endOfWeek, set} from 'date-fns';
+export default function WeeklyCalendar() {
+    const [date, setDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(endOfWeek(new Date(), { weekStartsOn: 1 }));
+    const [events , setEvents] = useState();
+    const showDetails = React.useRef(false);
+    const showDetailsHandle = (day,endDate) => {
+      setDate(day);
+      setEndDate(endDate);
+      showDetails.current = true; 
+    };
+
+    useEffect(() => {
+      (
+          async () => {
+              const filtersParams = [];
+
+              filtersParams.push(`company=${process.env.REACT_APP_COMPANY}`);
+              
+              if (date) {
+                  const dateFormat= format(date,"yyyy-MM-dd");
+                  filtersParams.push(`min_date=${dateFormat}`);
+              }
+
+              if (endDate) {
+                const endDateFormat= format(endDate,"yyyy-MM-dd");
+                  filtersParams.push(`max_date=${endDateFormat}`);
+              }
+
+              const response = await fetch(`${process.env.REACT_APP_BASE_URL}offer/?${filtersParams.join('&')}`);
+              const content = await response.json();
+
+              const object = content.results.reduce((r, { date_start: day, ...rest }) => {
+                day =new Date(day);
+                const key = format(day,"EEEE do MMMM");
+                const hour = format(day, "HH:mm")
+                if (!r[key]) r[key] = {key, data: [{hour, rest}]}
+                else r[key].data.push({hour,rest})
+                return r;
+              }, {})
+              
+              const events = Object.values(object);
+              setEvents(events);
+              //console.log(events);
+          }
+      )()
+
+    }, [date,endDate]);
+
+    return (
+      <div className="App">
+        <h1 className='text-center'>Calendar</h1>
+        <br />
+        <WeeklyCalendarRenderer showDetailsHandle={showDetailsHandle} />
+        <br />
+        {showDetails.current && events ? <WeeklyEvents events={events} /> : "There are no events for the selected dates"}
+      </div>
+    );
+}
